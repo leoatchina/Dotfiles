5a8e33156f7b80ccef7d05853338284c02db96af
leoatchina <leoatchina@outlook.com>
2025-06-18 10:06:38 -0400

    update

--- i3/install.sh
+++ i3/install.sh
@@ -1,2 +1,2 @@
 apt update -y && apt upgrade -y
-apt install -y wget curl git vim tmux ripgrep rofi i3-wm python3-dev python3-pip universal-ctags build-essential cmake autoconf
+apt install -y wget curl git vim tmux ripgrep rofi i3-wm python3-dev python3-pip universal-ctags build-essential cmake autoconf btop htop
