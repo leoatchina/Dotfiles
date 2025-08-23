57ea7e98590741a88353b9b008d2ed50eefd57df
leoatchina <leoatchina@outlook.com>
2025-08-23 17:56:34 +0800

    update

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -10,22 +10,35 @@ Import-Module posh-git
 Import-Module Terminal-Icons
 Import-Module scoop-completion
 oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh --config "amro" | Invoke-Expression
+
 
 
 # PSReadLine
 Import-Module PSReadLine
-Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineOption -BellStyle None
+
 Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -EditMode Emacs -BellStyle None -HistoryNoDuplicates -ShowToolTips
+Set-PSReadLineOption -Colors @{
+                        Command = 'Cyan'
+                        Parameter = 'Yellow'
+                        Operator = 'Red'
+                        Variable = 'Green'
+                        String = 'Magenta'
+                        Number = 'White'
+                        Member = 'DarkYellow'
+                        Type = 'DarkCyan'
+                        Default = 'Gray'
+                     }
 
 # Only set PredictionSource if PSReadLine version supports it (2.2.0+)
 if ($PSVersionTable.PSVersion.Major -ge 7 -or 
     (Get-Module PSReadLine).Version -ge [Version]"2.2.0") {
-    Set-PSReadLineOption -PredictionSource History
+    Set-PSReadLineOption -PredictionSource History -PredictionViewStyle ListView
 }
 # fzf
 Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r' -FzfOptions '--tac'
 # Utilities
 function which ($command) {
     Get-Command -Name $command -ErrorAction SilentlyContinue |
--- powershell/install.ps1
+++ powershell/install.ps1
@@ -1,9 +1,7 @@
 Set-ExecutionPolicy RemoteSigned -scope CurrentUser
-winget install -e --id Git.Git
 # powershell 安装依赖的方式 Install-Module，也可使用 Scoop 安装
-Install-Module oh-my-posh -Scope CurrentUser -Force
-Install-Module posh-git -Scope CurrentUser -Force
-Install-Module -Name Terminal-Icons -Repository PSGallery -Scope CurrentUser -Force
-Install-Module -Name z -Force -Scope CurrentUser
-Install-Module -Name PSReadLine -AllowPrerelease -Scope CurrentUser -Force -SkipPublishercheck
-Install-Module -Name PSFzf -Scope CurrentUser -Force -SkipPublishercheck
+Install-Module posh-git -Scope CurrentUser -AllowClobber -Force
+Install-Module -Name Terminal-Icons -Repository PSGallery -Scope CurrentUser -AllowClobber -Force
+Install-Module -Name z -Scope CurrentUser -AllowClobber -Force
+Install-Module -Name PSReadLine -Scope CurrentUser -AllowClobber -Force
+Install-Module -Name PSFzf -Scope CurrentUser -AllowClobber -Force
