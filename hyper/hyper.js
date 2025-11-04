cb60664ada16666f88fbf78a55cdf4060fc82733
leoatchina <leoatchina@outlook.com>
2025-11-04 18:14:50 +0800

    hyper

--- hyper/hyper.js
+++ hyper/hyper.js
@@ -137,7 +137,13 @@ module.exports = {
     //   `hyperpower`
     //   `@company/project`
     //   `project#1.0.1`
-    plugins: [],
+    plugins: [
+        "hyper-manager",
+        "hyperline",
+        "hyperterm-tabs",
+        "hyper-search",
+        "hyper-subliminal-theme"
+    ],
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
