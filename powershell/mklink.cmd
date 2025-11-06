4a786971f5502dcc92677d40b1337f9799a1b10c
leoatchina <leoatchina@outlook.com>
2025-11-06 18:06:39 +0800

    make double links

--- powershell/mklink.cmd
+++ powershell/mklink.cmd
@@ -1,5 +1,5 @@
 @echo off
-setlocal
+setlocal EnableDelayedExpansion
 
 rem Source in current script directory
 set "SCRIPT_DIR=%~dp0"
@@ -7,25 +7,22 @@ set "SCRIPT_DIR=%SCRIPT_DIR:~0,-1%"
 
 set "source=%SCRIPT_DIR%\Microsoft.PowerShell_profile.ps1"
 
-rem Destination under user Documents\PowerShell
-set "targetDir=%USERPROFILE%\Documents\WindowsPowerShell"
-set "dest=%targetDir%\Microsoft.PowerShell_profile.ps1"
+for %%D in ("%USERPROFILE%\Documents\WindowsPowerShell" "%USERPROFILE%\Documents\PowerShell") do (
+  set "targetDir=%%~D"
+  set "dest=%%~D\Microsoft.PowerShell_profile.ps1"
 
-rem Ensure destination directory exists
-if not exist "%targetDir%" mkdir "%targetDir%"
+  rem Ensure destination directory exists
+  if not exist "!targetDir!" mkdir "!targetDir!"
 
-rem Remove existing file or directory at destination
-if exist "%dest%\NUL" (
-  rmdir /S /Q "%dest%"
-) else if exist "%dest%" (
-  del /F /Q "%dest%"
+  rem Remove existing file or directory at destination
+  if exist "!dest!\NUL" (
+    rmdir /S /Q "!dest!"
+  ) else if exist "!dest!" (
+    del /F /Q "!dest!"
+  )
+
+  rem Create symbolic link (file)
+  mklink "!dest!" "%source%"
+  echo Created symbolic link: "!dest!" -> "%source%"
 )
 
-rem Create symbolic link (file)
-mklink "%dest%" "%source%"
-if errorlevel 1 (
-  echo Failed to create symbolic link. Try running as Administrator or enabling Developer Mode.
-  exit /b 1
-) else (
-  echo PowerShell profile linked successfully!
-)
