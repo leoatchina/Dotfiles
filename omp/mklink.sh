3f046acd816a56742cd4d2a65881a29e3feb2f4b
leoatchina <leoatchina@outlook.com>
2026-08-16 23:36:42 +0800

    fixed ln -sf bug

--- omp/mklink.sh
+++
@@ -1,4 +0,0 @@
-rm ~/.omp/agent/keybindings.yml || true
-rm ~/.omp/agent/config.yml || true
-ln -sf "$(dirname "$0")/agent/keybindings.yml" ~/.omp/agent/keybindings.yml && echo "=== omp keybindings linked ==="
-ln -sf "$(dirname "$0")/agent/config.yml" ~/.omp/agent/config.yml && echo "=== omp config linked ==="
