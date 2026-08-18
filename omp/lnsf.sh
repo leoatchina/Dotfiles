b4135e92b8e3126bab45ce2caa9620e3ef9e0311
leoatchina <leoatchina@outlook.com>
2026-08-18 19:31:35 +0800

    add cp config.yml

--- omp/lnsf.sh
+++ omp/lnsf.sh
@@ -3,3 +3,4 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 rm ~/.omp/agent/keybindings.yml || true
 ln -sf "$DIR/agent/keybindings.yml" ~/.omp/agent/keybindings.yml && echo "=== omp keybindings linked ==="
+[ ! -f ~/.omp/agent/config.yml ] && cp "$DIR/agent/config.yml" ~/.omp/agent  && echo "=== omp config copied ==="
